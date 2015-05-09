e90fae91623f3aaed04eb6511c520bc2a0e591e6
Ondra Kratochvíl <ondrej.kratochvil@keyup.eu>
2015-05-09 16:59:03 +0200

    initial commit

---
+++ src/robot/Robot.java
@@ -0,0 +1,17 @@
+package robot;
+
+/**
+ * Created by Ondřej Kratochvíl on 9.5.15.
+ */
+public class Robot {
+
+    public static void main(String[] args) {
+        if (args.length == 1) {
+
+        } else if (args.length == 2) {
+
+        } else {
+            System.out.println("Usage: ");
+        }
+    }
+}
